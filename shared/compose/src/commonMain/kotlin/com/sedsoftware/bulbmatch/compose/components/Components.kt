c075f6bea2efe00d72f80e409806822bf0189787
DJ_Kovrik <djkovrik@gmail.com>
2026-07-31 22:13:20 +0300

    Tweak appbar color

--- shared/compose/src/commonMain/kotlin/com/sedsoftware/bulbmatch/compose/components/Components.kt
+++ shared/compose/src/commonMain/kotlin/com/sedsoftware/bulbmatch/compose/components/Components.kt
@@ -38,6 +38,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -104,6 +105,9 @@ fun AppScreenScaffold(
         contentWindowInsets = WindowInsets.safeDrawing,
         topBar = {
             TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                ),
                 navigationIcon = {
                     if (onBack != null) {
                         val isClose = navigationIcon == AppNavigationIcon.Close
